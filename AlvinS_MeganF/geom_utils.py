ada702d8a28e4ad1cdf2a9dbae7d533a45d1e435
Alvin Sum <sum00005@algonquinlive.com>
2022-10-04 15:18:48 -0400

    1519

--- AlvinS_MeganF/geom_utils.py
+++ AlvinS_MeganF/geom_utils.py
@@ -1,18 +1,13 @@
-from operator import truediv
-from pickle import TRUE
-
-
 def is_point_in_box(x, y, xmin, ymin, xmax, ymax):
     if x > xmin and x < xmax:
         return True
-    if y > ymin and y < ymax:
-        return True
-    if x == xmin or x == xmax or y == ymin or y == ymax:
-        return False
+    elif y > ymin and y < ymax:
+         return True
+    elif x == xmin or x == xmax or y == ymin or y == ymax:
+         return False
     else:
-        return ()
+        return False
 
-    
 
 
 
--- AlvinS_MeganF/test_geom_utils.py
+++ AlvinS_MeganF/test_geom_utils.py
@@ -1,48 +1,51 @@
 import geom_utils as gu
 
+xmin = ymin = 1
+xmax = ymax = 5
+
 def test_is_point_in_box_LeftOfBox():
     expected = False
-    actual = gu.is_point_in_box()
+    actual = gu.is_point_in_box(.5, 5, xmin, ymin, xmax, ymax)
     assert expected == actual
 
 def test_is_point_in_box_OnLeftEdge():
     expected = False
-    actual = gu.is_point_in_box()
+    actual = gu.is_point_in_box(1, 5, xmin, ymin, xmax, ymax)
     assert expected == actual
 
 def test_is_point_in_box_AboveBox():
     expected = False
-    actual = gu.is_point_in_box()
+    actual = gu.is_point_in_box(1, 6, xmin, ymin, xmax, ymax)
     assert expected == actual
 
 def test_is_point_in_box_OnTopEdge():
     expected = False
-    actual = gu.is_point_in_box()
+    actual = gu.is_point_in_box(1, 5, xmin, ymin, xmax, ymax)
     assert expected == actual
 
 def test_is_point_in_box_RightOfBox():
     expected = False
-    actual = gu.is_point_in_box()
+    actual = gu.is_point_in_box(6, 5, xmin, ymin, xmax, ymax)
     assert expected == actual
 
 def test_is_point_in_box_OnRightEdge():
     expected = False
-    actual = gu.is_point_in_box()
+    actual = gu.is_point_in_box(5, 5, xmin, ymin, xmax, ymax)
     assert expected == actual
 
 def test_is_point_in_box_BelowBox():
     expected = False
-    actual = gu.is_point_in_box()
+    actual = gu.is_point_in_box(1, 0, xmin, ymin, xmax, ymax)
     assert expected == actual
 
 def test_is_point_in_box_OnBottomEdge():
     expected = False
-    actual = gu.is_point_in_box()
+    actual = gu.is_point_in_box(1, 1, xmin, ymin, xmax, ymax)
     assert expected == actual
 
 def test_is_point_in_box_InBox():
     expected = True
-    actual = gu.is_point_in_box()
+    actual = gu.is_point_in_box(2, 3, xmin, ymin, xmax, ymax)
     assert expected == actual
 
 
