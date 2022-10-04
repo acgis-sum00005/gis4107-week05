550a444b1f585ccbf7e5a631c689d5cf0250cc90
Alvin Sum <sum00005@algonquinlive.com>
2022-10-04 16:16:23 -0400

    1616

---
+++ AlvinS_MeganF/test_modvar.py
@@ -0,0 +1,21 @@
+import modvar as mod
+
+def test_mod_myvar_condition1():
+    expected = 9
+    actual = mod.mod_myvar(5)
+    assert expected == actual
+
+def test_mod_myvar_condition2():
+    expected = 2
+    actual = mod.mod_myvar(3)
+    assert expected == actual
+
+def test_mod_myvar_condition3():
+    expected = 4
+    actual = mod.mod_myvar(2)
+    assert expected == actual
+
+def test_mod_myvar_condition4():
+    expected = 10
+    actual = mod.mod_myvar(12)
+    assert expected == actual
