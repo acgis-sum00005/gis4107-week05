48211c260b8cc66835b66972a158368c1c53cf3a
Megan Fuller <full0085@algonquinlive.com>
2022-10-10 20:05:34 -0400

    excel_utilis attempt

--- AlvinS_MeganF/excel_utils.py
+++ AlvinS_MeganF/excel_utils.py
@@ -1,3 +1,6 @@
+rows = 1, 2, 3
+columns = "A, B, C"
 def create_cell_refs(columns, rows):
-    for col in columns.split():
-        
+    for col in range(columns):
+        for row in range(rows):
+
---
+++ AlvinS_MeganF/test_excel_utils.py
@@ -0,0 +1,6 @@
+import excel_utils as eu
+
+def test_create_cell_refs();
+    expected = "A1, A2, B1, B2"
+    actual = eu.create_cell_refs():
+    assert expected == actual
